cca458ff11e4a5d549acf4e2f42e9ed707760ac3
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-09 11:07:56 +0900

    chore: 빌드 설정 및 배포 자동화

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const port = 3001;
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(port);
